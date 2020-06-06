bb5714aa38342f19fc3978d93fc8e557335a76b3
MichaelLiu <michaelliu@C02ZRD0ZMD6N.local>
2020-06-07 01:07:12 +0800

    impl callback pool

---
+++ example/simple.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/yankooo/wasps"
+	"log"
+	"sync"
+	"time"
+)
+
+func main() {
+	pool := wasps.NewCallback(5)
+	defer func() {
+		pool.Release()
+	}()
+
+	var num = 10
+	ctx, _ := context.WithTimeout(context.TODO(), 1*time.Second)
+
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+	pool.SubmitWithContext(ctx, func(args ...interface{}) {
+		defer wg.Done()
+		num := args[0].(int)
+		log.Printf("first submit %d", num*2)
+	}, wasps.WithArgs(num))
+
+	pool.Submit(func(args ...interface{}) {
+		defer wg.Done()
+		num := args[0].(int)
+		log.Printf("second submit %d", num)
+	}, wasps.WithArgs(num), wasps.WithRecoverFn(func(r interface{}) { fmt.Printf("catch panic: %+v\n", r) }))
+
+	num = 200
+	pool.Submit(func(args ...interface{}) {
+		defer wg.Done()
+		log.Printf("third submit %d", num)
+	})
+
+	wg.Wait()
+}
---
+++ option.go
@@ -0,0 +1,71 @@
+package wasps
+
+type taskOptions struct {
+	RecoverFn func(r interface{})
+	Args      []interface{}
+}
+
+// TaskOption configures how we set up the connection.
+type TaskOption interface {
+	apply(*taskOptions)
+}
+
+// funcTaskOption wraps a function that modifies taskOptions into an
+// implementation of the TaskOption interface.
+type funcTaskOption struct {
+	f func(*taskOptions)
+}
+
+func (fdo *funcTaskOption) apply(do *taskOptions) {
+	fdo.f(do)
+}
+
+func newFuncTaskOption(f func(*taskOptions)) *funcTaskOption {
+	return &funcTaskOption{
+		f: f,
+	}
+}
+
+// WithRecoverFn returns task option for recover to catch panic
+func WithRecoverFn(f func(r interface{})) TaskOption {
+	return newFuncTaskOption(func(o *taskOptions) {
+		o.RecoverFn = f
+	})
+}
+
+// WithArgs returns task option for callback func args
+func WithArgs(args ...interface{}) TaskOption {
+	return newFuncTaskOption(func(o *taskOptions) {
+		o.Args = args
+	})
+}
+
+var defaultTaskOptions = &taskOptions{
+	RecoverFn: func(r interface{}) {},
+}
+
+type poolOptions struct {
+}
+
+// PoolOption configures how we set up the connection.
+type PoolOption interface {
+	apply(*poolOptions)
+}
+
+// funcPoolOption wraps a function that modifies poolOptions into an
+// implementation of the PoolOption interface.
+type funcPoolOption struct {
+	f func(*poolOptions)
+}
+
+func (fdo *funcPoolOption) apply(do *poolOptions) {
+	fdo.f(do)
+}
+
+func newFuncPoolOption(f func(*poolOptions)) *funcPoolOption {
+	return &funcPoolOption{
+		f: f,
+	}
+}
+
+var defaultPoolOptions = &poolOptions{}
